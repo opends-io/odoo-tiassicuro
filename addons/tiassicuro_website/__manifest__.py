7e514a81a6a0f8890d1b7c254ab78ef43bc5b0c2
mfornes <manuel.fornesmartinez@gmail.com>
2025-03-08 22:18:11 -0500

    Add module structure, init file manifest

---
+++ addons/tiassicuro_website/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Tiassicuro Website",
+    "version": "1.0",
+    "category": "Website",
+    "author": "Opends",
+    "depends": ['website'],    
+    "installable": True,
+    "application": False,
+}
