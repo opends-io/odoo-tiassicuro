a5d0dbc325efee8d9963b2248db3ee3b048a6a94
Yariel Rodriguez <yariel.re@gmail.com>
2025-03-10 23:34:07 +0100

    Update init & manifest files

---
+++ addons/website_tiassicuro/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Tiassicuro Website",
+    "version": "18.0.1",
+    "category": "Website/Theme",
+    "author": "Opends",
+    "depends": ["website"],
+    "installable": True,
+    "application": False,
+    "data": [
+        # ...
+    ],
+    "assets": {
+        # ...
+    },
+}
