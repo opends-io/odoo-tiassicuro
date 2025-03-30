cf7fa9de879f8495032a643ac5936786bf933c1d
hurshel <hurshelann30@gmail.com>
2025-03-30 04:10:26 -0400

    add Cassa Malati Svizzera page W.I.P

--- addons/tiassicuro_website/__manifest__.py
+++ addons/tiassicuro_website/__manifest__.py
@@ -11,8 +11,9 @@
     'data': [
         'views/home.xml',
         'views/assicurazione.xml',
-        'views/partners.xml',
         'views/protezione_giuridica.xml',
+        'views/cassa_malati.xml',
+        'views/partners.xml',
         'data/website_data.xml',
         'views/website_templates.xml',
     ],
@@ -20,8 +21,9 @@
         'web.assets_frontend': [
             'tiassicuro_website/static/src/css/home.css',
             'tiassicuro_website/static/src/css/assicurazione.css',
-            'tiassicuro_website/static/src/css/partners.css',
+            'tiassicuro_website/static/src/css/cassa_malati.css',
             'tiassicuro_website/static/src/css/protezione_giuridica.css',
+            'tiassicuro_website/static/src/css/partners.css',
         ],
     },
 }
