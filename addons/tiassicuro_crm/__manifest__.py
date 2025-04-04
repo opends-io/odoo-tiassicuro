178a8d2ecd15c0814fb59a9d2cc28139f33aab4e
mfornes <manuel.fornesmartinez@gmail.com>
2025-04-04 13:36:20 -0400

    Init tiassicuro_crm

---
+++ addons/tiassicuro_crm/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Tiassicuro CRM",
+    "description": "Customization for Tiassicuro CRM",
+    "version": "1.0",
+    "category": "Sales",
+    "author": "Opends",
+    "depends": ['tiassicuro_website', 'crm'],
+    "installable": True,
+    "application": False,
+}
