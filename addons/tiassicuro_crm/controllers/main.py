28aeb315ffed1155464665825f31d39c79aabefb
mfornes <manuel.fornesmartinez@gmail.com>
2025-04-07 00:49:41 -0400

    Add send email crm

--- addons/tiassicuro_crm/controllers/main.py
+++ addons/tiassicuro_crm/controllers/main.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 import logging
-from odoo import http
+from odoo import http, SUPERUSER_ID
 from odoo.http import request
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
 
 class CrmController(http.Controller):
+
+    def _send_email(self, id):
+        template = request.env['mail.template'].sudo().browse(request.env.ref('tiassicuro_crm.crm_mail_template').id)
+        if template:
+            template.with_user(SUPERUSER_ID).send_mail(id, force_send=True)
+        else:
+            raise UserError("Mail Template not found. Please check the template.")
+
     @http.route('/tiassicuro_crm/lead/quote', type='json', auth='public', methods=['POST'])
     def crm_lead_quote(self, **kwargs):
 
@@ -36,8 +45,11 @@ class CrmController(http.Controller):
             'partner_id': partner.id,
             'email_from': email,
             'phone': phone,
+            'company_id': request.env['res.company'].sudo().search([]).id,
             'description': description,
             'stage_id': request.env['crm.stage'].sudo().search([('name', '=', 'New')]).id,
         })
+
+        self._send_email(lead.id)
 
         return {'success': True, 'message': f'Insertado correctamente: {lead.id}.'}
